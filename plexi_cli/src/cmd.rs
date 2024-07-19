dd29900293d6aa50e40499b0f94569d2eb108ed3
Thibault Meunier <thibault@cloudflare.com>
2024-07-19 19:03:08 +0100

    Add signature version
    Signature are now versioned. This means their serialisation, or
    signature algorithm can be tuned down the line.
    
    This commit proposes two versions:
    * 0x0001 = protobuf and ed25519
    * 0x0002 = bincode and ed25519
    
    Version 0x0002 is the closest to the existing.
    
    The introduction of the version field is not backward compatible.
    Given the early stage of deployment, it's better to break now, rather
    than introduce a default non-versioned signature.

--- plexi_cli/src/cmd.rs
+++ plexi_cli/src/cmd.rs
@@ -1,14 +1,14 @@
 use std::{fs, io, path::PathBuf};
 
 // TODO: consider color_eyre, and chaining with [.context](https://docs.rs/anyhow/latest/anyhow/trait.Context.html#tymethod.context)
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
 use ed25519_dalek::{ed25519::signature::SignerMut, PUBLIC_KEY_LENGTH, SECRET_KEY_LENGTH};
 use plexi_core::{SignatureMessage, SignatureResponse};
 
 pub fn file_or_stdin(input: Option<PathBuf>) -> Result<Box<dyn io::Read>> {
     let reader: Box<dyn io::Read> = match input {
         Some(path) => Box::new(io::BufReader::new(
-            fs::File::open(path).map_err(|_e| anyhow!("cannot read input file"))?,
+            fs::File::open(path).context("cannot read input file")?,
         )),
         None => Box::new(io::BufReader::new(io::stdin())),
     };
@@ -18,22 +18,27 @@ pub fn file_or_stdin(input: Option<PathBuf>) -> Result<Box<dyn io::Read>> {
 pub fn file_or_stdout(output: Option<PathBuf>) -> Result<Box<dyn io::Write>> {
     let writer: Box<dyn io::Write> = match output {
         Some(path) => Box::new(io::BufWriter::new(
-            fs::File::create(path).map_err(|_e| anyhow!("cannot create output file"))?,
+            fs::File::create(path).context("cannot create output file")?,
         )),
         None => Box::new(io::BufWriter::new(io::stdout())),
     };
     Ok(writer)
 }
 
-pub fn sign(namespace: &str, signingkey: &str, output: Option<PathBuf>, input: Option<PathBuf>) -> Result<String> {
+pub fn sign(
+    namespace: &str,
+    signingkey: &str,
+    output: Option<PathBuf>,
+    input: Option<PathBuf>,
+) -> Result<String> {
     let src = file_or_stdin(input)?;
     let dst = file_or_stdout(output)?;
 
     let secret_key: [u8; SECRET_KEY_LENGTH] = hex::decode(signingkey)
-        .map_err(|_e| anyhow!("cannot decode signing key"))?
+        .context("cannot decode signing key")?
         .as_slice()
         .try_into()
-        .map_err(|_e| anyhow!("cannot convert signing key"))?;
+        .context("cannot convert signing key")?;
     let mut secret_key = ed25519_dalek::SigningKey::from_bytes(&secret_key);
 
     let message: SignatureMessage = serde_json::from_reader(src)?;
@@ -41,9 +46,10 @@ pub fn sign(namespace: &str, signingkey: &str, output: Option<PathBuf>, input: O
     let signature = secret_key.sign(&message.to_vec()?);
 
     let signature_response = SignatureResponse::new(
+        message.version(),
         namespace.to_string(),
         message.timestamp(),
-        &message.epoch(),
+        message.epoch(),
         message.digest(),
         signature.to_vec(),
     );
@@ -63,12 +69,12 @@ pub fn verify(namespace: &str, public_key: &str, input: Option<PathBuf>) -> Resu
     }
 
     let public_key: [u8; PUBLIC_KEY_LENGTH] = hex::decode(public_key)
-        .map_err(|_e| anyhow!("cannot decode public key"))?
+        .context("cannot decode public key")?
         .as_slice()
         .try_into()
-        .map_err(|_e| anyhow!("cannot convert public key"))?;
-    let public_key = ed25519_dalek::VerifyingKey::from_bytes(&public_key)
-        .map_err(|_| anyhow!("cannot create public key"))?;
+        .context("cannot convert public key")?;
+    let public_key =
+        ed25519_dalek::VerifyingKey::from_bytes(&public_key).context("cannot create public key")?;
 
     let signature = ed25519_dalek::Signature::from_bytes(&signature_response.signature());
 
@@ -76,7 +82,7 @@ pub fn verify(namespace: &str, public_key: &str, input: Option<PathBuf>) -> Resu
 
     public_key
         .verify_strict(&message.to_vec()?, &signature)
-        .map_err(|_e| anyhow!("cannot verify signature"))?;
+        .context("cannot verify signature")?;
 
     Ok("Signature valid".to_string())
 }
